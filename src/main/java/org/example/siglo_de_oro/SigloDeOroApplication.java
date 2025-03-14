c7f41c5aa45a36ac9a3509b901a8c2f70c81cc1b
산은철 <silverisiron@gmail.com>
2025-03-14 20:53:01 +0900

    🔧 chore: Add Files

---
+++ src/main/java/org/example/siglo_de_oro/SigloDeOroApplication.java
@@ -0,0 +1,13 @@
+package org.example.siglo_de_oro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigloDeOroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SigloDeOroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/siglo_de_oro/SigloDeOroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.siglo_de_oro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigloDeOroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
